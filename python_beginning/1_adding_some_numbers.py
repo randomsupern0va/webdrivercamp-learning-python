9532f32aaf55662f055fea661ee90fa0b3b5d343
Darianna Nauva <dariannanauva@Dariannas-MacBook-Air.local>
2024-05-14 03:14:28 -0500

    changed order in adding some numbers file to starting with most recent

--- python_beginning/1_adding_some_numbers.py
+++ python_beginning/1_adding_some_numbers.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 num = "100"
-print("Learning Python is fun\"\' - " + num + " %")
-print("Learning Python is fun\"\' - {} %".format(num))
 print(f"Learning Python is fun\"\' - {num} %")
+print("Learning Python is fun\"\' - {} %".format(num)
+print("Learning Python is fun\"\' - " + num + " %")
