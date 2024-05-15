1317c1580e899283dcf175f136209a7bcd1cdcf0
Darianna Nauva <dariannanauva@Dariannas-MacBook-Air.local>
2024-05-14 19:00:04 -0500

    pushing modify_numbers file and change in adding_numbers file

--- python_beginning/1_adding_some_numbers.py
+++ python_beginning/1_adding_some_numbers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-num = "100"
+num = 100
 print(f"Learning Python is fun\"\' - {num} %")
-print("Learning Python is fun\"\' - {} %".format(num)
-print("Learning Python is fun\"\' - " + num + " %")
+print("Learning Python is fun\"\' - {} %".format(num))
+print("Learning Python is fun\"\' - " + "%d %%" % num)
+#print("Learning Python is fun\"\' - " + num + " %")
---
+++ python_beginning/2_modify_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+num = 3.141592653589793238
+print(f"Learning Python is fun\"\' - {num:.2f} %")
+print("Learning Python is fun\"\' - {:.2f} %".format(num))
+print("Learning Python is fun\"\' - " + "%.2f %%" % num)
