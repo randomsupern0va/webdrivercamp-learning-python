b5354a025585d361ed0b035f6531f415688981a8
Darianna Nauva <dariannanauva@Dariannas-MacBook-Air.local>
2024-05-24 23:22:15 -0500

    adding max_value file for 3.1

---
+++ python-data-structures-continue/10_max_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def max_value(d):
+	if d:
+		return max(d, key=lambda k: d[k])			
+
+				
+			
+
+	
+
+if __name__=="__main__":
+
+    dict_ = {'Apple': 13, 'Pear': 1, 'Plum': 20, 'Grape': 10}
+    max_key = max_value(dict_)
+    print(f"Max number - {max_key}")
+    max_key = max_value(None)
+    print(f"Max number - {max_key}")
